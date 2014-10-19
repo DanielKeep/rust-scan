f687806f751c7a756bee5a765449390eea968b3d
Daniel Keep <daniel.keep@gmail.com>
2014-10-19 19:41:40 +1100

    Added documentation.
    Closes #14.

--- src/lib.rs
+++ src/lib.rs
@@ -30,7 +30,7 @@ Note that `scan_util` *must* be in the root module, and *must* not be renamed.  
 
 ## Usage
 
-Two macros are provided for scanning text.  In general, both are concerned with taking a string and matching it against a sequence of patterns.  As soon as a matching pattern is found, the corresponding expression is evaluated and returned.
+Several macros are provided for scanning text.  In general, they are concerned with taking a string and matching it against a sequence of patterns.  As soon as a matching pattern is found, the corresponding expression is evaluated and returned.
 
 If an error occurs during an underlying IO operation *or* none of the provided patterns matched, an `Err(scan_util::ScanError)` will be returned.
 
@@ -52,93 +52,44 @@ scan! {
 
 	"banana" => "definitely a banana",
 	"crowbar" => "big metal banana",
-	_ => "probably not a banana at all",
+	.._ => "probably not a banana at all",
 }
 ```
 
-## Patterns
+`scanln_from!` provides a way to scan a line of text from a source *other* than standard input.
 
-A pattern is a space-separated sequence of one or more of the following:
+```{ignore}
+scanln_from! {
+	some_reader,
 
-### Text
+	"say hello" => println!("Hi from a reader!"),
+	"exit" => return,
+}
+```
 
-These indicate a block of text which should be matched.  This matching is done by converting *both* text in the pattern *and* input text into a sequence of tokens, ignoring whitespace.  Each token is either a sequence of letters, a sequence of digits, or a single non-whitespace character.  All whitespace is ignored.
+Finally, you can implement custom scanners for any type using the `scanner!` macro.
 
-For example, the string `"Left4Dead is pretty fun..."` is tokenised into the sequence `"Left"`, `"4"`, `"Dead"`, `"is"`, `"pretty"`, `"fun"`, `"."`, `"."`, and `"."`.
+```{ignore}
+struct Boolish(pub bool);
 
-This behaviour also means that `"ab cd ef"` and `"ab" "cd" "ef"` are semantically equivalent, but `"ab c" "d ef"` is different, because of the break between `c` and `d`.
+scanner! {
+	Boolish,
 
-Also note that text comparisons are *case insensitive*.
+	"true" | "yes" | "on" | "1" | "はい" => Boolish(true),
+	"false" | "no" | "off" | "0" | "いいえ" => Boolish(false),
+}
+```
 
-In future, the tokeniser, whitespace policy, and case insensitivity will be configurable.
+There are some illustrative examples below.  There are also some larger demonstrative examples:
 
-### Capture
+- [`rust-scan-calc`](https://github.com/DanielKeep/rust-scan-calc) is a simple calculator that shows how you can parse infix arithmetic expressions.
+- [`rust-scan-wavefront`](https://github.com/DanielKeep/rust-scan-wavefront) is a Wavefront OBJ lexer.
 
-These are used to capture a value from the input.  They are written in the form `name: type` like a `let` binding.  The type constraint (`:type`) may be omitted *if* the usage of the captured value allows the type to be inferred.
-
-For example, `age:int` will capture an integer and store it in a binding called `age`.  `thingy` will capture a value, the type of which is to be determined by inference.
-
-An additional restriction is that you can only capture types which implement the `scan_util::Scanner` trait.  By default, scanners for the builtin integer and floating-point types are available as well as `bool` and `char`.  There is also a scanner for `&str` which will capture a single token.
-
-If you wish to supply your own scanning logic for a type, you will need to wrap it in another type (such as a structure) and implement the `Scanner` trait.
-
-### Alternate
-
-A pattern written as `"a" | "b"` will match *either* `a` or `b`.  You can chain as many alternates as you want.
-
-Note that alternates are tried in lexical order, and the first one to match "wins" and no others are tried.  For example, in the pattern `"a" | "a b"`, the second branch will never match.  Also note that as the scanner does not do backtracking, a failure to match later on in a pattern will not cause other alternatives to be tried.
-
-All captures inside an alternate are wrapped in `Option`, with unmatched captures being `None`.
-
-### Group
-
-You can create a sub pattern using parentheses.  This is mainly used for grouping alternates.  For example: `"a" ("b" | "c") "d"`; this will match `a b d` or `a c d`.
-
-### Repetition
-
-Reptitions are used to match a repeating sequence, and optionally capture values from said sequence.  The general form is `[pattern] separator repeat`.
-
-For example:
-
-* `["pretty"]+ "please"` - matches a childish plea.
-
-* `"rgb (" [cs:f32],{3} ")"` - matches a CSS `rgb` colour value.
-
-* `[ns:int](ws:&str)+` - matches a sequence of at least one number separated by arbitrary words.
-
-The `separator` is used to indicate what to match between instances of the main repeating `pattern`.  It can be omitted (in which case no seperator is scanned), or a sub pattern.  A sub pattern consisting of a single text literal can be written with the parentheses omitted.  If the text literal is `","`, `"."`, `";"`, or `":"`, then it may be written without the quotes as well.
-
-The `repeat` indicates how many times the repeating `pattern` must appear.  It can be any one of:
-
-- `?` - zero or one times.
-
-- `*` - zero or more times.
-
-- `+` - one or more times.
-
-- `{n}` - exactly `n` times.
-
-- `{n,}` - at least `n` times.
-
-- `{,m}` - zero or more, but no more than `m`, times.
-
-- `{n,m}` - at least `n`, but no more than `m`, times.
-
-All captures inside a repetition are inserted into `Vec`s.  Nesting repetitions yield nested `Vec`s.
-
-### Tail Capture
-
-Patterns must match their *entire* input.  If you only wish to perform a prefix match, you must add a tail capture to the pattern.  For example, `"spake thee thus", ..the_rest` would match "spake thee thus" and then capture the remainder of the input into `the_rest`.  Note the comma to distinguish the tail capture from the pattern.  Also note that tail captures are `&str`s, limited to the lifetime of the input.
-
-If you wish to do a prefix match, but do not actually care about the remainder of the input, you can use `_` as the capture name.
-
-### Fallback Pattern
-
-Finally, you can specify a fallback arm which *always* matches.  The complete pattern should be either a tail capture (such as `..tail` without any preceeding pattern), or a simple `_`.
+If you want more, the integration tests (in the `tests` directory) function as examples for (hopefully) every available feature.
 
 ## Example
 
-Below is a complete example of various kinds of patterns.
+Below is an example of various kinds of patterns.
 
 ```{ignore}
 #![feature(phase)]
@@ -226,9 +177,226 @@ fn identify_banana<S: Str>(s: S) -> &'static str {
 		"banana" => "definitely a banana",
 		"crowbar" => "giant metal banana",
 		"human finger" => "tiny crunchy banana",
+		"na"{8} => "Batman!",
 		_ => "probably not a banana"
 	}).unwrap()
 }
+```
+
+## Patterns
+
+A pattern is a sequence of one or more of the elements in the following sub-sections.
+
+Here is a quick summary of the available features:
+
+- Sequencing: `"set" n:int "."`.
+- Literal text: `"hi"` `"two words"` `"by default,   whitespace\tis\r\n  and\tcase\nare ignored."`.
+- Regular expressions: `/"[0-9]+"`.
+- Captures: `"vec3d:" x:f32 "," y:f32 "," z:f32`.
+- Slice captures: `"a string literal:" literal=/r#"^\"(\\\.|.)*\""#`.
+- Alternates: `"a or b:" ("a" | "b")`.
+- Repetition: `"usernames:" [names]+`.
+- Negative Lookahead Assertions: `"some words:" [(?! ".") anything]* "."`.
+- Tail capture: `"user-agent:", ..the_rest`.
+
+### Sequence
+
+Writing two or more matches side-by-side separated by whitespace causes them to be matched one after another, in turn.  For example, a pattern which matches the literal word `set`, captures an integer, then matches a full stop would be written as: `"set" n:int "."`.
+
+Note that the whitespace between pattern elements is ignored and is only *actually* required if its removal would change the pattern's meaning.
+
+### Text
+
+String literals indicate a block of text which should be matched.  This matching is done by converting *both* text in the pattern *and* input text into a sequence of tokens.  The precise semantics of this can be controlled using the `#[tokenizer="..."]` and `#[space="..."]` attributes (see below).
+
+For example, the string `"Left4Dead is pretty fun..."` is tokenised (under default settings) into the sequence `"Left"`, `"4"`, `"Dead"`, `"is"`, `"pretty"`, `"fun"`, `"."`, `"."`, and `"."`.
+
+Note that although you can have adjacent string literals in a pattern, they *are not* glued together prior to being tokenised.  This means that, in a pattern, `"ab cd ef"` and `"ab" "cd" "ef"` are semantically equivalent, but `"ab c" "d ef"` is distinct from them, because of the break between `c` and `d`.
+
+Also note that text comparisons are *case insensitive* by default.  This can be overriden using the `#[compare="..."]` attribute (see below).
+
+### Regular Expression
+
+If you need more control, you can also match against an arbitrary regular expression, as supported by the `regex` crate.  A regex is written as `/str`, where `str` is a string literal.  In most cases, you will want to use a raw string literal, which would look like `/r"..."`, or `/r##"..."##`.
+
+A regular expression is matched against a slice of the input *starting* at the current scan position, with all leading whitespace skipped, *until* the end of input.  In order for a regex to "match", it must find a match at the very beginning of this slice.  You can make matches faster by beginning your regex with `^`.
+
+For example, `"a" /"[0-9]+" "b"` will match "a 0 b", "a 42 b", and "a42b" (due to the default tokeniser).
+
+### Capture
+
+These are used to capture a value from the input.  They are written in the form `name: type` like a `let` binding.  The type constraint (`:type`) may be omitted *if* the usage of the captured value allows the type to be inferred.
+
+For example, `age:int` will capture an integer and store it in a binding called `age`.  `thingy` will capture a value, the type of which is to be determined by inference.
+
+An additional restriction is that you can only capture types which implement the `scan_util::Scanner` trait.  By default, scanners for the builtin integer and floating-point types are available as well as `bool` and `char`.  There are also scanners for `&str` and `String` which will capture a single token.
+
+You can use the `scanner!` macro to implement a custom scanner using the same scan pattern syntax as the other macros.  Alternately, you can implement the `Scanner` trait manually (see the documentation for the `scan_util` crate).
+
+### Slice Capture
+
+You can also capture the slice of the input which corresponds to a particular sub-pattern.  This is written as `name = pattern`.  For example, given the pattern `"a" bc=(["b"]+ | "c") "d"`, scanning `"a b   b \t b c"` will bind `"b   b \t b"` to `bc`.
+
+This is particularly useful in conjunction with alternates containing simple token matches (such as `("yes" | "no")`) and regular expressions.
+
+### Alternate
+
+A pattern written as `"a" | "b"` will match *either* `a` or `b`.  You can chain as many alternates as you want.
+
+Note that alternates are tried in lexical order, and the first one to match "wins" and no others are tried.  For example, in the pattern `"a" | "a b"`, the second branch will never match due to ordering.  Also note that as the scanner does not do backtracking, a failure to match later on in a pattern will not cause other alternatives to be tried.
+
+All captures inside an alternate are wrapped in `Option`, with unmatched captures being `None`.  This is true even if a capture is present in *all* alternates.
+
+### Group
+
+You can create a sub pattern using parentheses.  This is mainly used for grouping alternates.  For example: `"a" ("b" | "c") "d"`; this will match `a b d` or `a c d`.
+
+### Repetition
+
+Reptitions are used to match a repeating sequence, and optionally capture values from said sequence.  The general form is `[pattern] separator repeat`.
+
+For example:
+
+* `["pretty"]+ "please"` - matches a childish plea.
+
+* `"rgb (" [cs],{3} ")"` - matches a CSS `rgb` colour value, with comma-separated values.
+
+* `[ns:int](ws:&str)+` - matches a sequence of at least one number separated by arbitrary words, with both the numbers *and* words being captured.
+
+The `separator` is used to indicate what to match between instances of the main repeating `pattern`.  It can be omitted (in which case no seperator is scanned), or a sub pattern.  A sub pattern consisting of a single text literal can be written with the parentheses omitted.  If the text literal is `","`, `"."`, `";"`, or `":"`, then it may be written without the quotes as well.
+
+The `repeat` indicates how many times the repeating `pattern` must appear.  It can be any one of:
+
+- `?` - zero or one times.
+
+- `*` - zero or more times.
+
+- `+` - one or more times.
+
+- `{n}` - exactly `n` times.
+
+- `{n,}` - at least `n` times.
+
+- `{,m}` - zero or more, but no more than `m`, times.
+
+- `{n,m}` - at least `n`, but no more than `m`, times.
+
+All captures inside a repetition are inserted into `Vec`s.  Nesting repetitions yield nested `Vec`s.
+
+Note that the *entirety* of `pattern` must match for a specific iteration to "count".  If it only *partially* matches, then the scan cursor will be rolled back to the beginning of that specific iteration, and the repetition as a whole will end.
+
+### Negative Lookahead Assertion
+
+Because `scan` does not perform backtracking, the pattern `"a" [words] "b"` *cannot* be matched: the `[words]` repetition will *always* consume the ending `"b"` token.
+
+To get around this, you can use a negative lookahead assertion.  We can fix this example by rewriting it as `"a" [(?!"b") words] "b"`.  The repetition's pattern now reads as "match `words`, unless the next token matches `"b"`."
+
+More specifically, a NLA is written as `(?! pattern)`.  It *never* consumes any of the input, and only matches if the given `pattern` *does not* match.  As demonstrated above, this construct exists to help terminate repetitions.
+
+As an aside, NLAs are not always necessary.  For example, `"a" [words ints] "b"` does not need a NLA.
+
+### Tail Capture
+
+With the exception of the `scanner!` macro, patterns must match their *entire* input.  If you only wish to perform a prefix match, you must add a tail capture to the pattern.  For example, `"spake thee thus", ..the_rest` would match "spake thee thus" and then capture the remainder of the input into `the_rest`.  Note the comma to distinguish the tail capture from the pattern.  Also note that tail captures are `&str`s, limited to the lifetime of the input.
+
+If you wish to do a prefix match, but do not actually care about the remainder of the input, you can use `_` as the capture name.
+
+### Fallback Pattern
+
+Finally, you can specify a fallback arm which *always* matches.  The complete pattern should be a tail capture (such as `..tail` without any preceeding pattern).
+
+### Pattern Attributes
+
+There are several attributes you can set on individual patterns.  They can be written in any order prior to the start of a pattern.
+
+#### `#[tokenizer="..."]` and `#[runtime_tok="..."]`
+
+These set the tokenisers used to turn string literals in the pattern *and* the input string into a sequence of tokens.  Setting `#[tokenizer="..."]` automatically sets `#[runtime_tok="..."]` to the same tokeniser by default, although this can be overridden by setting `#[runtime_tok="..."]` explicitly.  In the case of the input string, tokenisation happens under two circumstances:
+
+1. When the pattern wants to match a literal token, the next token from the input string is popped and compared.
+2. When a type's scanner requests the next token.
+
+Currently, the second only happens with the `&str` and `String` scanners.
+
+You can set any of the following values:
+
+* `WordsAndInts` (default) - each token is *either* one or more characters with the `Alphabetic` Unicode property *or* one or more characters in the "Number" Unicode category.
+* `IdentsAndInts` - each token is *either* a Rust identifier *or* one or more characters in the "Number" category.
+* `SpaceDelimited` - each token is one or more characters which *do not* have the `White_Space` property.
+* `Explicit` - each quoted string literal is turned into a single token.  In the case of input, the *entire input string* becomes a single token.
+
+For `#[runtime_tok="..."]` you can also specify a path to a value or type that implements the `scan_util::Tokenizer` trait.
+
+#### `#[space="..."]` and `#[runtime_sp="..."]`
+
+These set the whitespace policies used for patterns and on input strings.  They control what whitespace to ignore, and what to turn into explicit tokens that must be matched.  The available policies are:
+
+* `Ignore` (default) - whitespace between tokens is ignored.
+* `ExplicitNewline` - end of line sequences are turned into an explicit token; all other whitespace is ignored.
+* `Explicit` - runs of whitespace are turned into a single `" "` token, individual end of line sequences are turned into `"\n"` tokens.
+* `ExplicitAny` - runs of whitespace and end of line sequences are all turned into a single explicit `" "` token.
+* `Exact` - all whitespace characters are turned into literal tokens.
+
+For `#[runtime_sp="..."]` you can also specify a path to a value or type that implements the `scan_util::Whitespace` trait.
+
+### `#[compare="..."]` and `#[runtime_cmp="..."]`
+
+These set the comparators used to determine if two strings "match".  The available comparators are:
+
+* `CaseInsensitive` (default) - ignore case differences between strings.  Unicode normalisation is *not* done.  Note that this is *not* currently correct; it works by lowercasing individual code points and does not account for code points that map to multiple code points when lowercased.  It also does not take the user's locale into account.  As such, the exact behaviour of this setting may change in the future.
+* `AsciiCaseInsensitive` - ignore case differences between strings for code points in the ASCII range.  Unicode normalisation is *not* done.
+* `Exact` - strings will be compared for exact, binary equality.  Unicode normalisation is *not* done.
+
+For `#[runtime_cmp="..."]` you can also specify a path to a value or type that implements the `scan_util::CompareStrs` trait.
+
+**Note**: currently, `#[compare="..."]` does not do anything, as tokens are never compared at compile-time.
+
+### Pattern Grammar
+
+The following is the grammar used to parse patterns.
+
+```{notrust}
+<pattern> := <attributes> <alternates>
+
+<attributes> := <attribute> <attributes>?
+
+<alternates> := <sequence> ("|" <alternates>)?
+
+<sequence> := <sequence-atom> <sequence-atom>?
+
+<sequence-atom> := <maybe-capture> | <non-capture-atom>
+
+<non-capture-atom> := <maybe-text> | <maybe-regex> | <maybe-group> | <repetition>
+
+<maybe-text> := <text> "?"?
+
+<text> := <string_literal> | <raw_string_literal>
+
+<maybe-regex> := <regex> "?"?
+
+<regex> := "/" <text>
+
+<maybe-capture> := <capture> "?"?
+
+<capture> := <identifier> ( <slice-capture> | <constraint>? )
+
+<slice-capture> := "=" <non-capture-atom>
+
+<constraint> := ":" <type>
+
+<maybe-group> := <group> "?"?
+
+<group> := "(" <lookahead>? <alternates> ")"
+
+<lookahead> := "?" "!"
+
+<repetition> := "[" <alternates> "]" <separator>? <repeat-range>
+
+<separator> := "," | "." | ";" | ":" | <text> | <group>
+
+<repeat-range> := "?" | "*" | "+" | <numeric-range>
+
+<numeric-range> := "{" "," <uint> "}" | "{" <uint> ("," <uint>)? "}"
 ```
 */
 #![feature(if_let)]
@@ -248,6 +416,7 @@ mod plugin;
 
 mod util;
 
+#[doc(hidden)]
 #[plugin_registrar]
 pub fn plugin_registrar(reg: &mut rustc::plugin::Registry) {
 	reg.register_macro("scan", plugin::expand_scan);
